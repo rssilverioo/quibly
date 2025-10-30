bd81add983d076a470dd12b8ec22452f570e936b
Rodrigo Silvério <104869030+rssilverioo@users.noreply.github.com>
2025-10-29 21:01:40 -0300

    phone

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,17 +14,16 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Quibly",
-  description: "Your teacher",
+  title: "Quibly -  Turn your lessons into learning",
+  description: "Turn your lessons into learning",
   manifest: "/site.webmanifest",
   icons: {
     icon: [
-      { url: "/favicon.ico", sizes: "any" },
       { url: "/favicon-16x16.png", type: "image/png", sizes: "16x16" },
       { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
     ],
     apple: "/apple-touch-icon.png",
-    shortcut: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
     other: [
       {
         rel: "android-chrome",
